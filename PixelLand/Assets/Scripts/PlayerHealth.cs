66c6cc769f823352639187af79bc1d243b7c9cfd
Ofir David <ofir.david30@gmail.com>
2024-02-24 22:28:17 -0500

    hearts fixed and functional

--- PixelLand/Assets/Scripts/PlayerHealth.cs
+++ PixelLand/Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 10;
+    public int maxHealth = 6;
     public int health;
+    public GameObject[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    public Sprite halfHeart;
     void Start()
     {
         health = maxHealth;
@@ -19,9 +23,28 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        UpdateHearts();
         if(health <= 0)
         {
             GameObject.Destroy(gameObject);
+        }
+    }
+    void UpdateHearts()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health / 2)
+            {
+                hearts[i].GetComponent<SpriteRenderer>().sprite = fullHeart;
+            }
+            else if (i == health / 2 && health % 2 != 0)
+            {
+                hearts[i].GetComponent<SpriteRenderer>().sprite = halfHeart;
+            }
+            else
+            {
+                hearts[i].GetComponent<SpriteRenderer>().sprite = emptyHeart;
+            }
         }
     }
 }
